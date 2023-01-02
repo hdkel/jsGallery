846dea31471467ae77033c751d624c590dc55feb
James Hu <jhu@Jamess-MacBook-Pro.local>
2023-01-01 20:28:12 -0500

    Re-organize file structure;

--- jg/ping-pong-board/script.js
+++
@@ -1,8 +0,0 @@
-import { PingPongBoard } from './board.js';
-
-window.onload = () => {
-	// By default, we should have one frame.
-	new PingPongBoard({
-		target: document.getElementById('ping-pong-board')
-	});
-};
--- jg/script.js
+++ jg/script.js
@@ -1,8 +1,32 @@
-import { GalleryFrame } from './frame.js';
+import { GalleryFrame } from './js-gallery/frame.js';
+import { PingPongBoard } from './ping-pong-board/board.js';
 
-window.onload = () => {
-	// By default, we should have one frame.
+const app = document.getElementById('app');
+
+// Hook the jsGallery button
+const btnGallery = document.getElementById('app-js-gallery');
+btnGallery.onclick = () => {
+	emptyDom(app);
 	new GalleryFrame({
-		target: document.getElementById('jsGallery')
+		target: document.getElementById('app')
 	});
 };
+
+// Hook the score board button
+const btnScoreBoard = document.getElementById('app-js-ping-pong-board');
+btnScoreBoard.onclick = () => {
+	emptyDom(app);
+	new PingPongBoard({
+		target: document.getElementById('app')
+	});
+}
+
+/**
+ * Helper method to clear all children from a DOM
+ * @param {Element} element
+ */
+const emptyDom = (element) => {
+	while(element.firstChild){
+		element.removeChild(element.firstChild);
+	}
+}
