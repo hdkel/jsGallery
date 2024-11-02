3349301a2f67c49c2ff655cb032e0f48f46f8dc2
jhu <jameshu325@gmail.com>
2024-11-01 21:47:53 -0400

    restart gallery

--- src/js-gallery-2/gallery.js
+++ src/js-gallery-2/gallery.js
@@ -1,6 +1,23 @@
+import {hashCode} from "../utility.js";
+import {Frame} from "./frame.js";
+
 export class Gallery {
+
     constructor(args) {
         const { target } = args;
-        target.append('js2');
+
+        this._layout = [];
+        this._frames = [];
+        this._initFrameData();
+    }
+
+    _initFrameData() {
+        const id = hashCode(5);
+        const frame = {
+            id,
+            bgColor: Frame.pickColor(),
+        };
+        this._layout.push(id);
+        this._frames.push(frame);
     }
 }
